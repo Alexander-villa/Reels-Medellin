9a35a5f103e818353e5b7a1058c950467be7adf4
Edwin Alexander Villa Taborda <edwin.villa@infra.noblenexus.net>
2023-12-06 17:14:38 -0500

    Add js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var videos = document.querySelectorAll("video");
-
-  videos.forEach(function(video) {
-      video.addEventListener("play", function() {
-          pauseOtherVideos(video);
-      });
-
-      video.addEventListener("click", function() {
-          if (video.requestFullscreen) {
-              video.requestFullscreen();
-          } else if (video.mozRequestFullScreen) {
-              video.mozRequestFullScreen();
-          } else if (video.webkitRequestFullscreen) {
-              video.webkitRequestFullscreen();
-          }
-      });
-  });
-
-  function pauseOtherVideos(currentVideo) {
-      videos.forEach(function(video) {
-          if (video !== currentVideo) {
-              video.pause();
-          }
-      });
-  }
-});
-
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -35,15 +6,3 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
-  let options = {
-    numberPerPage:2, //Cantidad de datos por pagina
-    goBar:true, //Barra donde puedes digitar el numero de la pagina al que quiere ir
-    pageCounter:true, //Contador de paginas, en cual estas, de cuantas paginas
-};
-
-let filterOptions = {
-    el:'#searchBox' //Caja de texto para filtrar, puede ser una clase o un ID
-};
-
-paginate.init('.myTable',options,filterOptions);
